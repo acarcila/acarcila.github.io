cc39ba64889062a4778b366abd2d797aceb7e5cb
Andrés Camilo Arcila <acarcila98@gmail.com>
2020-02-02 10:40:20 -0500

    Se programó la actualización de la edad según la fecha actual

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 $(document).ready(function () {
-    var $cursor = $('#cursor');
+    var $edad = $('#edad');
 
-    document.addEventListener('mousemove', function (e) {
-        var x = e.clientX;
-        var y = e.clientY;
+    var timestampNacimiento = 905990400 *1000; //17/09/1998 00:00
 
-        $cursor.css({ "left": x + "px", "top": y + "px" });
-    });
+    var edadAnios = calcularEdad(timestampNacimiento);
+
+    $edad.text(edadAnios);
 });
+
+function calcularEdad(timestampNacimiento){
+    var timestampActual = new Date().getTime();
+    return Math.floor(((timestampActual - timestampNacimiento)  / (1000 * 60 * 60 * 24 * 365)))
+}
+
+// $(document).ready(function () {
+//     var $cursor = $('#cursor');
+
+//     document.addEventListener('mousemove', function (e) {
+//         var x = e.clientX;
+//         var y = e.clientY;
+
+//         $cursor.css({ "left": x + "px", "top": y + "px" });
+//     });
+// });
+
