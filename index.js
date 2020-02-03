ea9ccd5c75c18b19195fdfc52222afbfa8404960
Andrés Camilo Arcila <acarcila98@gmail.com>
2020-02-02 23:00:55 -0500

    Se reemplazó Jquery por DOM en JS

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-$(document).ready(function () {
-    var $edad = $('#edad');
+document.addEventListener("DOMContentLoaded", function (event) {
+    var elementEdad = document.getElementById("edad");
 
-    var timestampNacimiento = 905990400 *1000; //17/09/1998 00:00
+    var timestampNacimiento = 905990400 * 1000; //17/09/1998 00:00
 
     var edadAnios = calcularEdad(timestampNacimiento);
 
-    $edad.text(edadAnios);
+    elementEdad.textContent = edadAnios;
 });
 
-function calcularEdad(timestampNacimiento){
+function calcularEdad(timestampNacimiento) {
     var timestampActual = new Date().getTime();
-    return Math.floor(((timestampActual - timestampNacimiento)  / (1000 * 60 * 60 * 24 * 365)))
+    return Math.floor(((timestampActual - timestampNacimiento) / (1000 * 60 * 60 * 24 * 365)))
 }
-
-// $(document).ready(function () {
-//     var $cursor = $('#cursor');
-
-//     document.addEventListener('mousemove', function (e) {
-//         var x = e.clientX;
-//         var y = e.clientY;
-
-//         $cursor.css({ "left": x + "px", "top": y + "px" });
-//     });
-// });
-
