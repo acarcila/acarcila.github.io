7f899859058f6f84aeaa1fbe9b1d7e9e4a35e3ad
Andrés Camilo Arcila <acarcila98@gmail.com>
2020-05-31 13:04:20 -0500

    Se corrigieron algunos estilos y se agregó el cambio del menú según el scroll

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+    var lastId,
+        topMenu = $("#menu"),
+        topMenuHeight = topMenu.outerHeight(),
+        // All list items
+        menuItems = topMenu.find(".o-boton-lateral"),
+        // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function () {
+            var item = $($(this).attr("href"));
+            if (item.length) { return item; }
+        });
+
     var elementEdad = document.getElementById("edad");
 
     var timestampNacimiento = 905990400 * 1000; //17/09/1998 00:00
@@ -6,19 +17,41 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var edadAnios = calcularEdad(timestampNacimiento);
 
     elementEdad.textContent = edadAnios;
-});
 
-calcularEdad = (timestampNacimiento) => {
-    var timestampActual = new Date().getTime();
-    return Math.floor(((timestampActual - timestampNacimiento) / (1000 * 60 * 60 * 24 * 365)))
-}
 
-goToSection = (sectionID, boton) => {
-    var arrElements = document.getElementsByClassName('o-boton-lateral');
-    for (var i = 0; i < arrElements.length; i++) {
-        arrElements[i].classList.remove("selected");
+    menuItems.click(function (e) {
+        var href = $(this).attr("href"),
+            offsetTop = href === "#inicio" ? 0 : $(href).offset().top;
+        $('html, body').stop().animate({
+            scrollTop: offsetTop
+        }, 300);
+        e.preventDefault();
+    });
+
+    function calcularEdad(timestampNacimiento) {
+        var timestampActual = new Date().getTime();
+        return Math.floor(((timestampActual - timestampNacimiento) / (1000 * 60 * 60 * 24 * 365)))
     }
-    var botonID = boton.id;
-    document.getElementById(sectionID).scrollIntoView();
-    document.getElementById(botonID).classList.add("selected");
-}
+
+    $(window).scroll(function () {
+        // Get container scroll position
+        var fromTop = $(this).scrollTop() + topMenuHeight;
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function () {
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length - 1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id) {
+            lastId = id;
+            // Set/remove active class
+            menuItems
+                .removeClass("active")
+                .filter("[href='#" + id + "']").addClass("active");
+        }
+    });
+});
